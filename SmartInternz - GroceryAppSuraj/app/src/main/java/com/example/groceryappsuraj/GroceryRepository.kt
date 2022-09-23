1b25a6640e6090259c58e13b05d8619590f8a4a0
Suraj Mapari <suraj02mapari@gmail.com>
2022-09-24 00:51:51 +0530

    first commit

---
+++ SmartInternz - GroceryAppSuraj/app/src/main/java/com/example/groceryappsuraj/GroceryRepository.kt
@@ -0,0 +1,9 @@
+package com.example.groceryappsuraj
+
+class GroceryRepository (private val db: GroceryDatabase) {
+
+    suspend fun insert(items: GroceryItems) = db.getGroceryDao().insert(items)
+    suspend fun delete(items: GroceryItems) = db.getGroceryDao().delete(items)
+
+    fun getAllItems() = db.getGroceryDao().getAllGroceryItems()
+}
---
+++ SmartInternz - GroceryAppSuraj/app/src/main/java/com/example/groceryappsuraj/GroceryViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.groceryappsuraj
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class GroceryViewModel (private val repository: GroceryRepository) : ViewModel() {
+
+    fun insert(items: GroceryItems) = GlobalScope.launch {
+        repository.insert(items)
+    }
+
+    fun delete(items: GroceryItems) = GlobalScope.launch {
+        repository.delete(items)
+    }
+
+    fun getAllGroceryItems() = repository.getAllItems()
+
+}
